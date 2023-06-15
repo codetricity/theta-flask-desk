5e2190d96492cf0d332c6c269d7cf33b82195ef3
Craig <craig@oppkey.com>
2023-06-15 14:13:12 -0700

    added self time 5

--- routes/image_settings.py
+++ routes/image_settings.py
@@ -13,3 +13,15 @@ def delay3():
     # response = create_response(payload)
     response = requests.post(base_url + 'commands/execute', json=payload)
     return render_template('image_settings.html', response = response.text, title='self timer set to 3')
+
+@app.route('/delay5')
+def delay5():
+    payload = {"name": "camera.setOptions",
+               "parameters": {
+                   "options": {
+                       "exposureDelay": 5
+                   }
+               }}
+    # response = create_response(payload)
+    response = requests.post(base_url + 'commands/execute', json=payload)
+    return render_template('image_settings.html', response = response.text, title='self timer set to 5')
