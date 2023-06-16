10effb5453258fae835e7088618d4ee893040b10
Craig <craig@oppkey.com>
2023-06-15 17:33:08 -0700

    move to video size sets mode move to image settings set to image

--- routes/page.py
+++ routes/page.py
@@ -1,5 +1,7 @@
-from app import app
+from app import app, base_url
 from flask import render_template
+import requests
+from time import sleep
 
 @app.route('/')
 @app.route('/index')
@@ -8,8 +10,26 @@ def home():
 
 @app.route('/video_size')
 def video_size():
+    payload = {"name": "camera.setOptions",
+               "parameters": {
+                   "options": {
+                       "captureMode": "video"
+
+                   }
+               }}
+    response = requests.post(base_url + 'commands/execute', json=payload)
+    sleep(0.75)
     return render_template('video_size.html', title='video size format')
 
 @app.route('/image_settings')
 def image_settings():
+    payload = {"name": "camera.setOptions",
+            "parameters": {
+                "options": {
+                    "captureMode": "image"
+
+                }
+            }}
+    response = requests.post(base_url + 'commands/execute', json=payload)
+    sleep(0.75)
     return render_template('image_settings.html', title='image settings')
