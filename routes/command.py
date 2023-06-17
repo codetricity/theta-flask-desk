d81926859551ab019a44771c84901d71b5c1c367
Craig <craig@oppkey.com>
2023-06-16 18:23:14 -0700

    added active menu to home sidebar

--- routes/command.py
+++ routes/command.py
@@ -10,78 +10,79 @@ from time import sleep
 #     output = json.dumps(response.json(), indent=2)
 #     return render_template('response.html', response = output)
 
-@app.route('/take_picture')
+
+@app.route("/take_picture")
 def take_picture():
     payload = {"name": "camera.takePicture"}
     # response = create_response(payload)
-    response = requests.post(base_url + 'commands/execute', json=payload)
+    response = requests.post(base_url + "commands/execute", json=payload)
     sleep(0.75)
-    return render_template('index.html', response = response.text, title='taking picture')
+    return render_template(
+        "index.html",
+        response=response.text,
+        title="taking picture",
+        active_nav="take_picture",
+    )
 
-@app.route('/last_image')
+
+@app.route("/last_image")
 def last_image():
     url = f"{base_url}commands/execute"
     command_string = "camera.listFiles"
     payload = {
-                "name": command_string,
-                "parameters": {
-                    "fileType": "image",
-                    "entryCount": 1,
-                    "maxThumbSize": 0
-
-                }}
-    resp = requests.post(
-                        url,
-                        json=payload)
+        "name": command_string,
+        "parameters": {"fileType": "image", "entryCount": 1, "maxThumbSize": 0},
+    }
+    resp = requests.post(url, json=payload)
 
     data = resp.json()
     last_file_url = data["results"]["entries"][0]["fileUrl"]
-    name = data["results"]["entries"][0]["name"]    
-    return render_template('index.html', image_url = last_file_url, title= name + ' last image')
+    name = data["results"]["entries"][0]["name"]
+    return render_template(
+        "index.html",
+        image_url=last_file_url,
+        title=name + " last image",
+        active_nav="last_image",
+    )
 
-@app.route('/full_image')
+
+@app.route("/full_image")
 def full_image():
-    image_url = request.args.get('image_url')
-    name = request.args.get('name')
+    image_url = request.args.get("image_url")
+    name = request.args.get("name")
     print(image_url, name)
-    return render_template('image.html', image_url = image_url, title=name + ' image in camera')
+    return render_template(
+        "image.html", image_url=image_url, title=name + " image in camera"
+    )
 
-@app.route('/ten_images')
+
+@app.route("/ten_images")
 def ten_images():
     url = f"{base_url}commands/execute"
     command_string = "camera.listFiles"
     payload = {
-                "name": command_string,
-                "parameters": {
-                    "fileType": "image",
-                    "entryCount": 10,
-                    "maxThumbSize": 0
-
-                }}
-    resp = requests.post(
-                        url,
-                        json=payload)
+        "name": command_string,
+        "parameters": {"fileType": "image", "entryCount": 10, "maxThumbSize": 0},
+    }
+    resp = requests.post(url, json=payload)
 
     data = resp.json()
-    entries = data["results"]["entries"]    
-    return render_template('thumbs.html', entries = entries, title="Files on camera")
+    entries = data["results"]["entries"]
+    return render_template(
+        "thumbs.html", entries=entries, title="Files on camera"
+    )
 
-@app.route('/thirty_images')
+
+@app.route("/thirty_images")
 def thirty_images():
     url = f"{base_url}commands/execute"
     command_string = "camera.listFiles"
     payload = {
-                "name": command_string,
-                "parameters": {
-                    "fileType": "image",
-                    "entryCount": 30,
-                    "maxThumbSize": 0
-
-                }}
-    resp = requests.post(
-                        url,
-                        json=payload)
+        "name": command_string,
+        "parameters": {"fileType": "image", "entryCount": 30, "maxThumbSize": 0},
+    }
+    resp = requests.post(url, json=payload)
 
     data = resp.json()
-    entries = data["results"]["entries"]    
-    return render_template('thumbs.html', entries = entries, title="Files on camera")
+    entries = data["results"]["entries"]
+    return render_template("thumbs.html", entries=entries, title="Files on camera")
--- routes/protocol.py
+++ routes/protocol.py
@@ -3,26 +3,18 @@ import requests
 import json
 from flask import render_template, jsonify
 
-# def create_response(protocol, name):
-#     if protocol == 'get':
-#         response = requests.get(base_url + name)
-#     elif protocol == 'post':
-#         response = requests.post(base_url + name)
-#     output = json.dumps(response.json(), indent=2)
-#     return render_template('response.html', response = output)
 
-@app.route('/info')
+@app.route("/info")
 def info():
-    try:
-        response = requests.get(base_url + 'info')
-        return render_template('index.html', response = response.text, title='camera info')
-    except:
-        return render_template('index.html', response = 'error.  please true again')
+    response = requests.get(base_url + "info")
+    return render_template(
+        "index.html", response=response.text, title="camera info", active_nav="info"
+    )
 
-@app.route('/state')
+
+@app.route("/state")
 def state():
-    try: 
-        response = requests.post(base_url + 'state')
-        return render_template('index.html', response = response.text, title='camera state')
-    except:
-        return render_template('index.html', response = 'error.  please true again')    
+    response = requests.post(base_url + "state")
+    return render_template(
+        "index.html", response=response.text, title="camera state", active_nav="state"
+    )
